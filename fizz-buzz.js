e61f760da581772afb2acad575c76fe12701847f
Michael <tharratt.michael@gmail.com>
2022-09-22 09:47:14 -0700

    Learning about DOM manipulation

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,3 +1,29 @@
+// declare container variables
+const container = document.querySelector('#container');
+// declare child variables
+const para = document.createElement('p');
+const heading3 = document.createElement('h3');
+// create class for child variables
+para.classList.add('redPara');
+heading3.classList.add('blueHeading');
+// create text content for child variables
+para.textContent = "Hey I'm red!";
+heading3.textContent = "I'm a blue H3!";
+// inject text into child variables
+container.appendChild(para);
+container.appendChild(heading3);
+// style variables
+para.style.color = 'red';
+heading3.style.color = "blue";
+
+
+
+
+
+
+
+/*
+
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
@@ -11,3 +37,5 @@ for (let i = 1; i <= answer; i++) {
       console.log(i);
     }
   }
+
+  */
