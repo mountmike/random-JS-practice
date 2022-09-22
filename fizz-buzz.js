4f2359a39af12920c3a43ce860e30a37205f77ce
Michael <tharratt.michael@gmail.com>
2022-09-22 09:58:38 -0700

    Created nested elements inside a div container, added text and styles.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,22 +1,38 @@
 // declare container variables
 const container = document.querySelector('#container');
+
 // declare child variables
 const para = document.createElement('p');
 const heading3 = document.createElement('h3');
+const divContainer = document.createElement('div');
+const heading1 = document.createElement('h1');
+const subPara = document.createElement('p');
+
 // create class for child variables
 para.classList.add('redPara');
 heading3.classList.add('blueHeading');
+divContainer.classList.add('divContainer');
+heading1.classList.add('h1Div')
+subPara.classList.add('subPara');
+
+
 // create text content for child variables
 para.textContent = "Hey I'm red!";
 heading3.textContent = "I'm a blue H3!";
+heading1.textContent = "I'm in a div";
+subPara.textContent = "Me TOO!";
+
 // inject text into child variables
 container.appendChild(para);
 container.appendChild(heading3);
+container.appendChild(divContainer);
+divContainer.appendChild(heading1);
+divContainer.appendChild(subPara);
+
 // style variables
 para.style.color = 'red';
 heading3.style.color = "blue";
-
-
+divContainer.style.cssText = "background-color: pink; border: 3px black solid";
 
 
 
